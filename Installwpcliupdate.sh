42845ec1c14343208cedaf447602e86584266c47
Adrian Estrada <adrian.estrada@vincoorbis.com>
2016-10-03 16:09:31 -0500

    Script para Instalar Script de Actualizacion WP-CLI

--- Installwpcliupdate.sh
+++ Installwpcliupdate.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-cd /root/
-mkdir .scripts
-cd /root/.scripts
-curl -O https://raw.githubusercontent.com/adrianestrada/SASAAS/master/Linux/updatewpcli.sh
-chmod +x updatewpcli.sh
-echo "0 0 * * 0 root /root/.scripts/updatewpcli.sh" >> /etc/crontab
+WPROUTE="/root/.scripts/"
+VHOSTS="/var/www/vhosts/"
+echo -n "Deseas Instalar el script y/n [ENTER]: "
+read script
+if [$script = y]; then
+	cd /root/
+	mkdir .scripts
+	cd /root/.scripts 
+	curl -O https://raw.githubusercontent.com/adrianestrada/SASAAS/master/Linux/updatewpcli.sh
+	chmod +x updatewpcli.sh
+	echo "0 0 * * 0 root /root/.scripts/updatewpcli.sh" >> /etc/crontab
+if [$script = n]
+	echo -n "Deseas Actualizar el WP-Cli y/n [ENTER]"
+	read update
+	if [$read = y]; then
+		cd $WPROUTE
+		curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+		find $VHOSTS -name "wp-cli*" -type d -exec cp $WPROUTE/wp-cli.phar {}/bin/ \;
+	else
+fi
