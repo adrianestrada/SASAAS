2c252f17a2ac59615a39f85c83becbb1d9f442c4
Adrian Estrada <adrian.estrada@vincoorbis.com>
2016-10-03 15:10:16 -0500

    Script para instalar Server Wordpress

--- Linux/updatewpcli.sh
+++ Linux/updatewpcli.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 WPROUTE="/root/.scripts/"
 VHOSTS="/var/www/vhosts/"
+cd $WPROUTE
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 find $VHOSTS -name "wp-cli*" -type d -exec cp $WPROUTE/wp-cli.phar {}/bin/ \;
